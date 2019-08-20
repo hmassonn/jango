e9af1f854eda85645d7df1ee28de30d27dd9f9c5
olag18 <hmassonn@e1r5p6.42.fr>
2019-08-20 21:20:03 +0200

    404

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+from django.template import loader
+from .models import Product
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, product_id):
+    # try:
+    #     product = Product.objects.get(pk=product_id)
+    # except Product.DoesNotExist:
+    #     raise Http404("product does not exist")
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'polls/detail.html', {'product': product})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, product_id):
+    response = "You're looking at the results of product %s."
+    return HttpResponse(response % product_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-from .models import Product
+def vote(request, product_id):
+    return HttpResponse("You're voting on product %s." % product_id)
 
 def index(request):
-    latest_question_list = Product.objects.all()
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_product_list = Product.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_product_list': latest_product_list,
+    }
+    return HttpResponse(template.render(context, request))
