50642280156aad8fbcc903354e60e7daa59e2ada
olag18 <hmassonn@e1r13p3.42.fr>
2019-08-21 18:03:16 +0200

    final modif

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+docker start djngo
+eval $(docker-machine env django)
+docker run -P --name pg_test eg_postgresql
+docker container ls
+# modify mysite/settings.py
+python3 manage.py runserver 8080
 python3 manage.py createsuperuser
 python3 manage.py makemigrations polls
 python3 manage.py migrate
-docker run -P --name pg_test eg_postgresql
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse
 from django.template import loader
-from .models import Product
+from django.urls import reverse
+from django.views import generic
+
+from .models import Product, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Product.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Product
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Product
+    template_name = 'polls/results.html'
+
+
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
@@ -15,11 +38,26 @@ def detail(request, product_id):
     return render(request, 'polls/detail.html', {'product': product})
 
 def results(request, product_id):
-    response = "You're looking at the results of product %s."
-    return HttpResponse(response % product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'polls/results.html', {'product': product})
 
 def vote(request, product_id):
-    return HttpResponse("You're voting on product %s." % product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    try:
+        selected_choice = product.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the product voting form.
+        return render(request, 'polls/detail.html', {
+            'product': product,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(product.id,)))
 
 def index(request):
     latest_product_list = Product.objects.order_by('-pub_date')[:5]
