ec59ce61f1100defe59a7810ba8201699f2e288a
olag18 <hmassonn@e1r7p3.42.fr>
2019-08-29 19:06:25 +0200

    migration

--- polls/models.py
+++ polls/models.py
@@ -1,23 +1,31 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    title = models.CharField(max_length=30) 
     name = models.CharField(max_length=30)
 
 class Product(models.Model):
-    title = models.CharField(max_length=30)
-    image = models.CharField(max_length=200)
-    price = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, default='unsee')
+    image = models.CharField(max_length=200, default='unsee')
+    price = models.CharField(max_length=30, default='unsee')
+    description = models.CharField(max_length=200, default='unsee')
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, default='unsee')
     # question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.title
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Command(models.Model):
+    login = models.CharField(max_length=30) 
+    basket = models.CharField(max_length=30)
+    date = models.DateTimeField('date published')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Choice(models.Model):
     question = models.ForeignKey(Product, on_delete=models.CASCADE)
